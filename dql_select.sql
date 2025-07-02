3c8334160503786970890aacf268753e924a9904
Arenas07 <189859279+Arenas07@users.noreply.github.com>
2025-07-02 07:27:43 -0500

    feat: :sparkles: Ejercicio 2 select terminado

--- dql_select.sql
+++ dql_select.sql
@@ -10,3 +10,11 @@ INNER JOIN sucursal suc ON e.sucursalid = suc.id
 INNER JOIN municipio mun ON mun.id = suc.municipioid
 INNER JOIN departamento dep ON dep.id = mun.depid
 INNER JOIN pais pa ON pa.id = dep.paisid;
+
+-- Listar el nombre de cada cliente con su municipio, departamento y país.
+
+SELECT cl.nombre, mun.nombre, dep.nombre, pa.nombre
+FROM clientes cl
+INNER JOIN municipio mun ON cl.municipioid = mun.id
+INNER JOIN departamento dep ON dep.id = mun.depid
+INNER JOIN pais pa ON pa.id = dep.paisid;
