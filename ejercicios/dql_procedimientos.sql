5890396fadaae32da9bbc94d565f7b454cef4aee
Arenas07 <189859279+Arenas07@users.noreply.github.com>
2025-07-02 08:21:49 -0500

    feat: :sparkles: Ejercicio 2 procedimientos terminado

--- ejercicios/dql_procedimientos.sql
+++ ejercicios/dql_procedimientos.sql
@@ -21,7 +21,7 @@ DELIMITER ;
 
 CALL ps_registrar_cliente_unico(
   'Adrian Ruiz', 
-  'adrian.ruiz@gmail.com', 
+  'adrian.rddduiz@gmail.com', 
   '3123456789', 
   'Calle 123 #45-67', 
   '2025-02-01', 
@@ -32,4 +32,22 @@ SELECT * FROM clientes WHERE cliente_id = LAST_INSERT_ID();
 
 
 
--- 
+-- Cree un procedimiento por nombre `ps_obtener_clientes_por_municipio` donde liste todos los clientes de un municipio.
+
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS ps_obtener_clientes_por_municipio;
+CREATE PROCEDURE ps_obtener_clientes_por_municipio(
+    IN p_municipio VARCHAR(80)
+    )
+BEGIN
+    SELECT cl.cliente_id, cl.nombre, cl.email
+    FROM municipio mun
+    INNER JOIN clientes cl ON mun.id = cl.municipioid
+    WHERE mun.nombre = 'Cali';
+
+END //
+
+DELIMITER ;
+
+CALL ps_obtener_clientes_por_municipio('Cali');
