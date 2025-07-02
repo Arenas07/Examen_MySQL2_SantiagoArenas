6a0bda12b76cc734c6c88ee0450cb6c55d6038b8
Arenas07 <189859279+Arenas07@users.noreply.github.com>
2025-07-02 09:34:38 -0500

    feat: :sparkles: Ejercicio 3 eventos terminado

--- ejercicios/dql_eventos.sql
+++ ejercicios/dql_eventos.sql
@@ -30,3 +30,34 @@ BEGIN
 END //
 
 DELIMITER ;
+
+-- Define un evento llamado `ev_cierre_empleados_inactivos` que actualiza estado de empleados con salario 0 como inactivos, semanal.
+
+ALTER TABLE empleados ADD COLUMN estado VARCHAR(80);
+
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS ev_cierre_empleados_inactivos;
+CREATE PROCEDURE ev_cierre_empleados_inactivos()
+BEGIN
+
+    UPDATE empleados
+    SET estado = 'inactivo'
+    WHERE salario = 0;
+
+END //
+
+DELIMITER ;
+
+
+DELIMITER //
+
+DROP EVENT IF EXISTS ev_cierre_empleados_inactivos;
+CREATE EVENT ev_cierre_empleados_inactivos
+ON SCHEDULE EVERY 1 WEEK
+DO 
+BEGIN
+    CALL ev_cierre_empleados_inactivos();
+END //
+
+DELIMITER ;
